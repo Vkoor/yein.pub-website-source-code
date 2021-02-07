4cfe72f156b3f20817d931d0cd547f6b97ab81c7
yeinenterprice <78648817+yeinenterprice@users.noreply.github.com>
2021-02-07 10:36:11 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+<link href="https://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="ROBOTS" content="NOINDEX, FOLLOW" />
+<title>Info / 夜莺慕柒中文资源论坛</title>
+<link rel="stylesheet" type="text/css" href="style/Cobalt.css" />
+
+<style type="text/css">.gid1 {color: #b4e61e; font-weight: bold} .gid2 {color: #ff9933; font-weight: bold} .gid5 {color: #e61515; font-weight: bold} </style>
+<link href="https://fonts.googleapis.com/css?family=Raleway:900,400" rel="stylesheet" type="text/css">
+
+<link rel="stylesheet" type="text/css" href="/static/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<link rel="icon" href="favicon.ico" /> 
+
+<!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="favicon.ico" /><![endif]-->
+</head>
+
+<body>
+<nav class="gs-navbar">
+	<div class="gs-nav-container">
+		<ul>
+			<li><a href="/"><i class="fa fa-home"></i></a></li>
+			<li class="active"><a href="/forums">论坛</a></li>
+		</ul>
+	</div>
+</nav>
+<div class="gs-divider"></div>
+
+<body>
+
+<div id="punindex" class="pun">
+<div class="top-box"></div>
+<div class="punwrap">
+<div id="brdheader" class="block">
+	<div class="box">
+		<div id="brdtitle" class="inbox">
+			<h1><a href="index.php">夜莺<span>慕柒</span></a></h1>
+			<div id="brddesc"><p><span>夜莺慕柒中文资源论坛</span></p></div>
+		</div>
+		<div id="brdmenu" class="inbox">
+			<ul>
+				<li id="navindex" class="isactive"><a href="http://sqziyuan.xyz/home.php">主页</a></li>
+				<li id="navregister" class="isactive"><a href="http://sqziyuan.xyz/member.php?action=register">注册</a></li>
+				<li id="navlogin" class="isactive"><a href="http://sqziyuan.xyz/member.php?action=login">登录</a></li>
+			</ul>
+		</div>
+		<div id="brdwelcome" class="inbox">
+			<div class="clearer"></div>
+		</div>
+	</div>
+</div>
+
+
+
+
+<div id="brdmain">
+<div id="msg" class="block">
+	<h2><span>提示</span></h2>
+	<div class="box">
+		<div class="inbox">
+			<p>点击“主页”按钮转到论坛主页</p>
+			<p><a href="javascript: history.go(-1)">返回</a></p>
+		</div>
+	</div>
+</div>
+</div>
+
+<div id="brdfooter" class="block">
+	<div class="box">
+		<div class="inbox">
+			<p>夜莺慕柒中文资源论坛 2020</p>
+			<ul>
+				<li><span><a href="tos.php">服务使用条款 / 隐私政策</a></span></li>
+			</ul>
+			<div class="clearer"></div>
+		</div>
+	</div>
+</div>
+
+</div>
+<div class="end-box"></div>
+</div>
+
+</body>
+</html>
